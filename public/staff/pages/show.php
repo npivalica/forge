7b6192cd9657aef46ca9366829854778e6ad8e9c
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2022-03-02 22:45:55 +0100

    fixing forgotten require from initialize

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,3 +1,4 @@
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
 // $id = isset($_GET['id']) ? $_GET['id'] : '1';
 $id = $_GET['id'] ?? '1'; // PHP > 7.0
@@ -8,13 +9,13 @@ $id = $_GET['id'] ?? '1'; // PHP > 7.0
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+    <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="page show">
+    <div class="page show">
 
-    Page ID: <?php echo htmlspecialchars($id); ?>
+        Page ID: <?php echo htmlspecialchars($id); ?>
 
-  </div>
+    </div>
 
 </div>
 
