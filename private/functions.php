4608cf851be443782209f77a0cbcf4e1c0db86ea
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2022-03-03 12:22:42 +0100

    Headers, page redirection and output buffering

--- private/functions.php
+++ private/functions.php
@@ -8,5 +8,12 @@ function url_for($script_path)
     }
     return WWW_ROOT . $script_path;
 }
-
+function error_404() {
+    header($_SERVER["SERVER_PROTOCOL"] . "404 NOT FOUND"); //render your own 404 page that you will render insetad of exit method
+    exit();
+}
+function error_500(){
+    header($_SERVER["SERVER_PROTOCOL"] . "500 INTERNAL SERVER ERROR"); 
+    exit();
+}
 ?>
--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();//output buffering turned on
 
   define("PRIVATE_PATH", dirname(__FILE__));
   define("PROJECT_PATH", dirname(PRIVATE_PATH));
