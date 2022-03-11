4f50ec881cb034c05595de4c6a878e73e564b1ac
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2022-03-11 23:54:04 +0100

    modification

--- private/shared/public_footer.php
+++ private/shared/public_footer.php
@@ -1,5 +1,6 @@
   <footer>
     <p>Copyright <?php echo date('Y'); ?>, forge </p>
+    <p><a href="<?php echo url_for('/dokumentacija.pdf'); ?>" target="_blank">Documentation</a></p>
     <p>This is a fictitious company created by <a href="<?php echo url_for('/author.php'); ?>">Nikolina Pivalica 9/17</a>, solely for the purposes of Web Programming PHP1 course.</p>
   </footer>
 
--- private/shared/static_author_page.php
+++ private/shared/static_author_page.php
@@ -5,10 +5,10 @@
   <h2>Nikolina Pivalica 9/17</h2>
 
   <div id="service-blocks">
-    <p>I'm at my final few exams at <a href="https://www.ict.edu.rs/" class="learnmore">ICT College of Vocational Studies</a> where I'm studying Web Programming. This is a project for PHP1 course. </p>
+    <p>I'm at my final few exams at <a href="https://www.ict.edu.rs/" class="learnmore" target="_blank">ICT College of Vocational Studies</a> where I'm studying Web Programming. This is a project for PHP1 course. </p>
     <p>I love learning new things and pushing myself out of my comfort zone, so I'm furthering my knowledge currently while I'm on a DevOps internship. Like Pokemon, I gotta learn it all!</p>
     <p>I'm an avid epic fantasy lover, cancer survivor, biggest Harry Potter fan you'll meet and a great teammate for trivia nights! </p>
-    <p><a href="https://www.linkedin.com/in/nikolina-pivalica/" class="learnmore">Learn more about me</a> </p>
+    <p><a href="https://www.linkedin.com/in/nikolina-pivalica/" class="learnmore" target="_blank">Learn more about me</a> </p>
 
 
   </div>
