179d63026f4e3b22fda7e6b0a0234f4c0015fe86
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2022-03-12 14:14:28 +0100

    seo improvements

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -1,27 +1,36 @@
 <!doctype html>
 
 <html lang="en">
-  <head>
-    <title>forge 
-      <?php 
-      if(isset($page_title)) { 
-        echo '- ' . htmlspecialchars($page_title);
-      }
-      if (isset($preview) && $preview) {
-        echo ' [PREVIEW]';
-      }
-      ?>
-      </title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
-  </head>
 
-  <body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="description" content="This is home page for forge bank where you can read all about our services and latest news." />
+  <meta name="keywords" content="bank, forge, loan, mortgage, home" />
+  <meta name="robots" content="noindex,follow" />
+  <meta name="language" content="english">
+  <title>forge
+    <?php
+    if (isset($page_title)) {
+      echo '- ' . htmlspecialchars($page_title);
+    }
+    if (isset($preview) && $preview) {
+      echo ' [PREVIEW]';
+    }
+    ?>
+  </title>
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
+  <link rel="shortcut icon" href="<?php echo url_for('favicon.ico'); ?>" />
 
-    <header>
-      <h1>
-        <a href="<?php echo url_for('/index.php'); ?>">
-          <img src="<?php echo url_for('/images/logo.png'); ?>"  alt="" />
-        </a>
-      </h1>
-    </header>
+</head>
+
+<body>
+
+  <header>
+    <h1>
+      <a href="<?php echo url_for('/index.php'); ?>">
+        <img src="<?php echo url_for('/images/logo.png'); ?>" alt="" />
+      </a>
+    </h1>
+  </header>
--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -9,9 +9,17 @@ if (!isset($page_title)) {
 <html lang="en">
 
 <head>
-  <title>forge - <?php echo htmlspecialchars($page_title); ?></title>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="This is forge staff area." />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="robots" content="noindex,follow" />
+  <meta name="language" content="english">
+  <meta name="keywords" content="cms, staff, subjects, pages, bank, forge" />
+  <title>forge - <?php echo htmlspecialchars($page_title); ?></title>
   <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
+  <link rel="shortcut icon" href="<?php echo url_for('favicon.ico'); ?>" />
+
 </head>
 
 <body>
