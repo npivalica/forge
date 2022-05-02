b7199588de1990289472ab6c8418dad02730af39
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2022-05-02 16:55:01 +0200

    added new service blocks to static homepage

--- private/shared/static_homepage.php
+++ private/shared/static_homepage.php
@@ -11,22 +11,44 @@
       <img src="images/homepage_assets/family_buying_home_L30707.jpg" width="250" height="166" alt="Family in front of sold home" />
       <h1>Buying a home?</h1>
       <p>There's no place like home, and there's no home loan like forge's.</p>
-      <p><a href="#" class="learnmore">Learn More...</a> </p>
     </div>
 
     <div class="service">
       <img src="images/homepage_assets/father_daughter_finances_L30717.jpg" width="250" height="166" alt="Father and daughter discussing finances" />
       <h1>Saving for a rainy day?</h1>
       <p>Let our financial experts help you commit to a savings plan and schedule that works for you.</p>
-      <p><a href="#" class="learnmore">Learn More...</a> </p>
     </div>
 
     <div class="service">
       <img src="images/homepage_assets/owner_salon_L30714.jpg" width="250" height="166" alt="Small business owner" />
       <h1>Starting a business?</h1>
       <p>From small business loans to merchant accounts, our service are designed to help your small business thrive.</p>
-      <p><a href="#" class="learnmore">Learn More...</a> </p>
     </div>
+
+    <div class="service">
+      <img src="images/homepage_assets/eduardosoaresutWPB8_FU8unsplash.jpg" width="250" height="166" alt="ATM" />
+      <h1>Easy access?</h1>
+      <p>There's a lot of ATMs over the country for easy access to your money.</p>
+    </div>
+
+    <div class="service">
+      <img src="images/homepage_assets/jasondent3wPJxhpiRwunsplash.jpg" width="250" height="166" alt="Safe" />
+      <h1>Secure and safe?</h1>
+      <p>Let our experts describe you in how many ways your savings are safe.</p>
+    </div>
+
+    <div class="service">
+      <img src="images/homepage_assets/micheiledotcomSoT4mZhhEunsplash.jpg" width="250" height="166" alt="Jar of money with a plant" />
+      <h1>Saving for college?</h1>
+      <p>We have a special service dedicated to young people starting college.</p>
+    </div>
+
+    <div class="service">
+      <img src="images/homepage_assets/pickawoodgf8e6XvG_3Eunsplash.jpg" width="250" height="166" alt="Online banking" />
+      <h1>Access from home?</h1>
+      <p>There's no place like home, and there's no internet banking like forge's.</p>
+    </div>
+
   </div>
 
 </div>
