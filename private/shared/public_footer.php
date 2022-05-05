9a1abf0cd9369073caa09c172a4fa9a171f594ea
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2022-05-05 20:22:30 +0200

    public footer

--- private/shared/public_footer.php
+++ private/shared/public_footer.php
@@ -7,5 +7,4 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="<?php echo url_for('/scripts/main.js'); ?>"></script>
   </body>
-
   </html>
